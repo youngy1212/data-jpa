441074adc2d82bb1449fb4dcdfab5f38f6eab470
youngy1212 <117425085+youngy1212@users.noreply.github.com>
2024-03-28 21:16:01 +0900

    순수 JPA 기반 Repository 만들기 (+CRUD TEST)

--- src/main/java/study/datajpa/repository/MemberJpaRepository.java
+++ src/main/java/study/datajpa/repository/MemberJpaRepository.java
@@ -6,6 +6,9 @@ import jakarta.persistence.PersistenceContext;
 import org.springframework.stereotype.Repository;
 import study.datajpa.entity.Member;
 
+import java.util.List;
+import java.util.Optional;
+
 @Repository
 public class MemberJpaRepository {
 
@@ -15,6 +18,24 @@ public class MemberJpaRepository {
     public Member save(Member member){
         em.persist(member);
         return member;
+    }
+
+    public void delete(Member member){
+        em.remove(member);
+    }
+
+    public List<Member> findAll(){
+        //JPQL 사용 (객체를 대상으로 하는 쿼리)
+        return em.createQuery("select m from Member m",Member.class).getResultList();
+    }
+
+    public Optional<Member> findById(Long id){ //null일 수 Member일 수 있음
+        Member member = em.find(Member.class, id);
+        return Optional.ofNullable(member);
+    }
+    public long count(){ //count long 반환
+        return em.createQuery("select count(m) from Member m",Long.class)
+                .getSingleResult();
     }
 
     public Member find(Long id){
